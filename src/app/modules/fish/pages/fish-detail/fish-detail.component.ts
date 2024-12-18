bb0753f4ba3c05ff7cfe4098fe6b513c2b8114b7
Carlos <carlosanzmunoz@gmail.com>
2024-12-18 14:16:54 +0100

    Detail Redirect aplied

--- src/app/modules/fish/pages/fish-detail/fish-detail.component.ts
+++ src/app/modules/fish/pages/fish-detail/fish-detail.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { FishDatum } from 'src/app/core/models/IFish.model';
+import { FishServiceService } from 'src/app/services/fish/fish-service.service';
 
 @Component({
   selector: 'app-fish-detail',
   templateUrl: './fish-detail.component.html',
-  styleUrls: ['./fish-detail.component.css']
+  styleUrls: ['./fish-detail.component.css'],
 })
 export class FishDetailComponent implements OnInit {
+  pescado: FishDatum | undefined;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private fishService: FishServiceService
+  ) {}
 
   ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id'); // El id ya es un string desde la URL
+    if (id) {
+      this.fishService.getFishById(id).subscribe(
+        (fish) => {
+          this.pescado = fish;
+          console.log(this.pescado); // Verifica que el pez se haya encontrado correctamente
+        },
+        (error) => {
+          console.error('Error al obtener el pez', error);
+        }
+      );
+    }
   }
-
 }
--- src/app/services/fish/fish-service.service.ts
+++ src/app/services/fish/fish-service.service.ts
@@ -1,7 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { IFish } from 'src/app/core/models/IFish.model';
+import { FishDatum, IFish } from 'src/app/core/models/IFish.model';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -11,5 +12,11 @@ export class FishServiceService {
 
   getAllFish(): Observable<IFish> {
     return this.httpClient.get<IFish>('assets/data/fishData.json');
+  }
+
+  getFishById(id: string): Observable<FishDatum | undefined> {
+    return this.httpClient.get<IFish>('assets/data/fishData.json').pipe(
+      map((response) => response.fishData.find((fish) => fish.id === id)) // Buscar por id dentro de fishData
+    );
   }
 }
