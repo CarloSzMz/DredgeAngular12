8397454c163e9f53aa639169f88c38cc020ec916
Carlos <carlosanzmunoz@gmail.com>
2024-12-18 10:12:14 +0100

    service configured

--- src/app/modules/fish/pages/fish-list/fish-list.component.ts
+++ src/app/modules/fish/pages/fish-list/fish-list.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { FishServiceService } from 'src/app/services/fish/fish-service.service';
 
 @Component({
   selector: 'app-fish-list',
   templateUrl: './fish-list.component.html',
-  styleUrls: ['./fish-list.component.css']
+  styleUrls: ['./fish-list.component.css'],
 })
 export class FishListComponent implements OnInit {
-
-  constructor() { }
+  constructor(private fishService: FishServiceService) {}
 
   ngOnInit(): void {
+    this.cargarDatos();
   }
 
+  cargarDatos() {
+    this.fishService.getAllFish().subscribe((response) => {
+      console.log(response);
+    });
+  }
 }
--- src/app/services/fish/fish-service.service.ts
+++ src/app/services/fish/fish-service.service.ts
@@ -1,9 +1,15 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { IFish } from 'src/app/core/models/IFish.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class FishServiceService {
+  constructor(private httpClient: HttpClient) {}
 
-  constructor() { }
+  getAllFish(): Observable<IFish> {
+    return this.httpClient.get<IFish>('assets/data/fishData.json');
+  }
 }
