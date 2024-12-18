252c617dc881c59a0f74f548851e745945063773
Carlos <carlosanzmunoz@gmail.com>
2024-12-18 14:14:17 +0100

    data updated

--- src/app/core/models/IFish.model.ts
+++ src/app/core/models/IFish.model.ts
@@ -1,31 +1,36 @@
 export interface IFish {
-    fishData: FishDatum[];
-  }
-  
-  export interface FishDatum {
-    id: string;
-    name: string;
-    location: Location;
-    type: string;
-    time: Time;
-    aberrations: string;
-    valuePerSlot: string;
-    grid: string;
-    image: string;
-  }
-  
-  export enum Location {
-    DevilSSpine = "Devil's Spine",
-    GaleCliffs = 'Gale Cliffs',
-    OpenOcean = 'Open Ocean',
-    StellarBasin = 'Stellar Basin',
-    TheMarrows = 'The Marrows',
-    ThePaleReach = 'The Pale Reach',
-    TwistedStrand = 'Twisted Strand',
-  }
-  
-  export enum Time {
-    Any = 'Any',
-    Day = 'Day',
-    Night = 'Night',
-  }
+  fishData: FishDatum[];
+}
+
+export interface FishDatum {
+  id: string;
+  name: string;
+  location: Location;
+  type: Type;
+  time: Time;
+  aberrations: string;
+  valuePerSlot: string;
+  grid: string;
+  image: string;
+}
+
+export enum Location {
+  DevilSSpine = "Devil's Spine",
+  GaleCliffs = 'Gale Cliffs',
+  OpenOcean = 'Open Ocean',
+  StellarBasin = 'Stellar Basin',
+  TheMarrows = 'The Marrows',
+  ThePaleReach = 'The Pale Reach',
+  TwistedStrand = 'Twisted Strand',
+}
+
+export enum Time {
+  Any = 'Any',
+  Day = 'Day',
+  Night = 'Night',
+}
+
+export interface Type {
+  name: string;
+  image: string;
+}
